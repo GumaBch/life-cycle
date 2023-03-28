8c408a466bf6a88accbcb80b883a532690f92661
Guma <guma.bch@gmail.com>
2023-03-28 12:13:30 -0300

    Dando vida aos componentes: aula 10: @Input() - comunicação de pai para filho

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,7 +7,9 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+
+  @Input() itemQueVaiSerEditado!: Item;
 
   valorItem!: string;
 
@@ -23,5 +26,11 @@ export class InputComponent implements OnInit {
 
   limpaCampo() {
     this.valorItem = '';
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if(!changes['itemQueVaiSerEditado'].firstChange) {
+      this.valorItem = this.itemQueVaiSerEditado?.nome;
+    }
   }
 }
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -21,8 +21,8 @@ export class ItemComponent implements OnInit, OnChanges {
 
   ngOnChanges(): void { }
 
-editarItem() {
-  this.emitindoItemParaEditar.emit(this.item);
-}
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
+  }
 
 }
